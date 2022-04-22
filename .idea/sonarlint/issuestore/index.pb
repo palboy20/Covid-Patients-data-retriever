ed2c7f409ddeae407ab5d46ff30f63f960c4f636
PalRanjeet <rp.hbtu@gmail.com>
2022-04-22 12:13:47 +0530

    added some changes

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+b
+2src/main/java/com/patient/api/main/PatientAPI.java,c\c\cc0c4e141eb1fedaf7993ed7055afe823a54f9fb
--- src/main/java/com/patient/api/main/PatientAPI.java
+++ src/main/java/com/patient/api/main/PatientAPI.java
@@ -3,7 +3,7 @@ package com.patient.api.main;
 /*
  * This class is responsible to invoke patientOperationMethod
  * We create the object of PatintOperation class to call the
- * patientOperationMethod that contains all four methods insert, retrieve, update and delete
+ * patientOperationMethod that contains all four method insert, retrieve, update and delete
  */
 public class PatientAPI 
 {
